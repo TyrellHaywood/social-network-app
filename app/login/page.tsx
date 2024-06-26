6e0369e7193c9e8a98f2e4797f8435b4e50090e7
TyrellHaywood <tyrelltman@gmail.com>
2024-06-26 00:11:42 -0400

    installing next auth and setting up auth route

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,22 +5,27 @@ export default function loginPage() {
   };
 
   return (
-    <form>
-      <input
-        type="email"
-        name="email"
-        placeholder="Email"
-        required
-        className="text-black"
-      />
-      <input
-        type="password"
-        name="password"
-        placeholder="Password"
-        required
-        className="text-black"
-      />
-      <button type="submit">Sign Up</button>
-    </form>
+    <div className="flex min-h-screen flex-row items-center justify-center">
+      <form
+        action={authenticate}
+        className="flex items-center justify-center flex-col"
+      >
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          required
+          className="text-black my-1 rounded block"
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          required
+          className="text-black my-1 rounded block"
+        />
+        <button type="submit">Log In</button>
+      </form>
+    </div>
   );
 }
