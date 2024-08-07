1c39e05de5634f2b9375db3a78922c00a53fa5b6
TyrellHaywood <tyrelltman@gmail.com>
2024-08-07 13:15:32 -0400

    updated options and route

---
+++ app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,63 @@
+import { NextApiHandler } from "next";
+import NextAuth, { type NextAuthOptions } from "next-auth";
+import GitHubProvider from 'next-auth/providers/github';
+import CredentialsProvider from "next-auth/providers/credentials";
+import prisma from "@/lib/prisma";
+import bcrypt, { compare } from "bcrypt";
+
+
+export const authOptions: NextAuthOptions = {
+    session: {
+        strategy: 'jwt'
+    },
+    providers: [
+        GitHubProvider({
+            clientId: process.env.GITHUB_ID as string,
+            clientSecret: process.env.GITHUB_SECRET as string,
+        }),
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                email: {
+                    label: "Email",
+                    type: "email",
+                    placeholder: "hello@example.com"
+                },
+                password: {
+                    label: "Password", 
+                    type: "password"
+                }
+            },
+            // handle auth!
+            async authorize(credentials) {
+                // check if credentials are correct
+                if(!credentials?.email || !credentials.password) {
+                    return null;
+                }
+
+                const user = await prisma.user.findUnique({
+                    where: {
+                        email: credentials.email
+                    }
+                })
+                // user does not exist
+                if (!user) {
+                    return null
+                }
+
+                const isPasswordValid = await compare(credentials.password, user.password)
+
+                if (!isPasswordValid) {
+                    return null
+                }
+
+                return {
+                    id: user.id +'',
+                    email: user.email,
+                    name: user.name
+                }
+            }
+        })
+    ],
+
+};
