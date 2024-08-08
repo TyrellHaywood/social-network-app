f919c5407e77e4e3050b827bd7df414462edfcea
TyrellHaywood <tyrelltman@gmail.com>
2024-08-08 19:08:01 -0400

    cleaning project structure

--- app/page.tsx
+++ app/page.tsx
@@ -9,13 +9,6 @@ export default async function Page() {
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
-      <pre>{JSON.stringify(session)}</pre>
-      <div className="w-40 m-1 rounded bg-white text-black flex justify-center">
-        <Link href="/api/auth/signin">Log In</Link>
-      </div>
-      <div className="w-40 m-1 rounded bg-white text-black flex justify-center">
-        <Link href="/signup">Sign Up</Link>
-      </div>
       <LoginButton />
       <LogoutButton />
     </div>
