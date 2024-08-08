a51348df281aa5afb7a0380fd253a439a621a59d
TyrellHaywood <tyrelltman@gmail.com>
2024-08-08 19:00:18 -0400

    adjusted / simplified project structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,23 @@
-import prisma from "@/lib/prisma";
-import Post from "./components/Post";
+"use server";
 import Link from "next/link";
 import { getServerSession } from "next-auth";
-import { authOptions } from "./api/auth/[...nextauth]/options";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { LoginButton, LogoutButton } from "@/app/auth";
 
-// async function getPosts() {
-//   const posts = await prisma.post.findMany({
-//     where: { published: true },
-//     include: {
-//       author: {
-//         select: { name: true },
-//       },
-//     },
-//   });
-//   return posts;
-// }
+export default async function Page() {
+  const session = await getServerSession(authOptions);
 
-export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <pre>{JSON.stringify(session)}</pre>
+      <div className="w-40 m-1 rounded bg-white text-black flex justify-center">
+        <Link href="/api/auth/signin">Log In</Link>
+      </div>
+      <div className="w-40 m-1 rounded bg-white text-black flex justify-center">
+        <Link href="/signup">Sign Up</Link>
+      </div>
+      <LoginButton />
+      <LogoutButton />
+    </div>
   );
 }
