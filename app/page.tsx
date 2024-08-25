904b73b6dd277cf70334ea60da8d362e44dfc42f
TyrellHaywood <tyrelltman@gmail.com>
2024-08-24 23:35:08 -0400

    fix: login forum

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ export default async function Page() {
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
+      <pre>{JSON.stringify(session)}</pre>
       <LoginButton />
       <LogoutButton />
     </div>
